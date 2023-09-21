44026b13817727f6d1ab5950d93b1f4a59ba4f94
Kevin Collins <ghcs2005@gmail.com>
2023-09-21 09:57:23 -0500

    GRADED ASSIGNMENTS PART 2: MULTIPLE QUESTIONS

--- candidate-testing.js
+++ candidate-testing.js
@@ -11,30 +11,44 @@ let candidateAnswer = "";
 
 
 //TODO: Variables for Part 2
-let questions;
-let correctAnswers;
+let questions = ["Who was the first American woman in space?", "True or false: 5 kilometer == 5000 meters?", "(5 + 3)/2 * 10 = ?", "[8, 'Orbit', 'Trajectory', 45], what entry is at index 2?", "What is the minimum crew size for the ISS?"];
+
+let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
 let candidateAnswers;
 
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-  candidateName = input.question("What is your name? ");
+  candidateName = input.question("What is your name?");
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-  input.question(question + candidateAnswer);
-
+  //input.question(question + candidateAnswer); 
+  
+for (let i = 0; i < questions.length; i++) {
+  if(questions == input.question(questions[i])){
+     console.log(i);
+    }
+  }  
 }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
- if(candidateAnswer == correctAnswer){
-  console.log("Your answer is correct! ")
- } else {
-  console.log("You entered the coorrect answer! ")
+  //correctAnswers.push("Sally Ride", true, 40, "Trajectory", "3");
+ for (i = 0; i < candidateAnswers.length; i++) {
+    console.log(i);
  }
+  for (i = 0; i < candidateAnswers.length; i++) {
+    console.log(i);
+ }
+
+ if(correctAnswers == candidateAnswers){
+      console.log(`${candidateAnswers} is correct!`)
+      } else {
+      console.log(`${candidateAnswers} is incorrect!`)
+     }
 
 let grade;  //TODO 3.2 use this variable to calculate the candidates score.
 
