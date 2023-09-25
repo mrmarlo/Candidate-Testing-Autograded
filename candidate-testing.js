c644b5643189aed30fa60c56e203dfa2454b5d57
Kevin Collins <ghcs2005@gmail.com>
2023-09-24 22:38:00 -0500

    PART 2 MULTIPLE QUESTIONS

--- candidate-testing.js
+++ candidate-testing.js
@@ -11,7 +11,13 @@ let candidateAnswer = "";
 
 
 //TODO: Variables for Part 2
-let questions = ["Who was the first American woman in space?", "True or false: 5 kilometer == 5000 meters?", "(5 + 3)/2 * 10 = ?", "[8, 'Orbit', 'Trajectory', 45], what entry is at index 2?", "What is the minimum crew size for the ISS?"];
+let questions = [
+("Who was the first American woman in space? "),
+("True or false: 5 kilometer == 5000 meters? "),
+("(5 + 3)/2 * 10 = ? "),
+("[8, 'Orbit', 'Trajectory', 45], what entry is at index 2? "),
+("What is the minimum crew size for the ISS? ")
+];
 
 let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
 let candidateAnswers;
@@ -19,7 +25,7 @@ let candidateAnswers;
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-  candidateName = input.question("What is your name?");
+  candidateName = input.question("What is your name? ");
 }
 
 function askQuestion() {
@@ -37,10 +43,10 @@ function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
   //correctAnswers.push("Sally Ride", true, 40, "Trajectory", "3");
- for (i = 0; i < candidateAnswers.length; i++) {
+ for (i = 0; i < candidateAnswers; i++) {
     console.log(i);
  }
-  for (i = 0; i < candidateAnswers.length; i++) {
+  for (i = 0; i < candidateAnswers; i++) {
     console.log(i);
  }
 
@@ -51,11 +57,12 @@ function gradeQuiz(candidateAnswers) {
      }
 
 let grade;  //TODO 3.2 use this variable to calculate the candidates score.
+   
+    
 
-
+    
   return grade;
 }
-
 function runProgram() {
   askForName();
   // TODO 1.1c: Greet candidate using their name //
@@ -64,6 +71,7 @@ function runProgram() {
   gradeQuiz(this.candidateAnswers);
 }
 
+//runProgram()
 // ----------- Don't write any code or change any code below this line ---------- //
 module.exports = {
   candidateName: candidateName,
